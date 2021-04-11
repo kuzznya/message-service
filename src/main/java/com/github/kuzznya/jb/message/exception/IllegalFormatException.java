11a849214357a997757f0a20b1f4dc34b2cc33aa
Ilya Kuznetsov <ikuz2002@gmail.com>
2021-04-11 19:14:05 +0300

    refactor: Remove unused constructors from exceptions

--- src/main/java/com/github/kuzznya/jb/message/exception/IllegalFormatException.java
+++ src/main/java/com/github/kuzznya/jb/message/exception/IllegalFormatException.java
@@ -5,19 +5,9 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class IllegalFormatException extends RuntimeException {
-    public IllegalFormatException() {
-        super("Illegal format");
-    }
-
-    public IllegalFormatException(String message) {
-        super(message);
-    }
 
     public IllegalFormatException(String message, Throwable cause) {
         super(message, cause);
     }
 
-    public IllegalFormatException(Throwable cause) {
-        super("Illegal format", cause);
-    }
 }
--- src/main/java/com/github/kuzznya/jb/message/exception/MessageSendException.java
+++ src/main/java/com/github/kuzznya/jb/message/exception/MessageSendException.java
@@ -5,16 +5,9 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.SERVICE_UNAVAILABLE)
 public class MessageSendException extends RuntimeException {
-    public MessageSendException() {
-        super("Message sending error");
-    }
 
     public MessageSendException(String message) {
         super(message);
-    }
-
-    public MessageSendException(String message, Throwable cause) {
-        super(message, cause);
     }
 
     public MessageSendException(Throwable cause) {
